1fe5f79ee3959187f6205dd9cbc75002b0c53de4
gogrene <gogreneinfo@gmail.com>
2022-09-12 13:53:49 +0100

    editted the frontend environment variables

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -6,9 +6,9 @@ export const environment = {
   production: false,
   apiServerUrl: 'http://127.0.0.1:5000', // the running FLASK api server url
   auth0: {
-    url: 'dev-z-5vcck9.us.auth0', // the auth0 domain prefix
+    url: 'dev-z-5vcck9.us', // the auth0 domain prefix
     audience: 'drinks', // the audience set for the auth0 app
     clientId: 'lZeBYPKa0nSVOOiIDIEZ6bAnDe14KQid', // the client id generated for the auth0 app
-    callbackURL: 'https://127.0.0.1:8100/login/tabs/user-page', // the base url of the running ionic application. 
+    callbackURL: 'https://127.0.0.1:8100/login', // the base url of the running ionic application. 
   }
 };
