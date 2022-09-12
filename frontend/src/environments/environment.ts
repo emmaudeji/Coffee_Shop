bc30a600f6eed43df4b7fcb2bce64d32097c7797
gogrene <gogreneinfo@gmail.com>
2022-09-12 07:11:40 +0100

    fronten variables

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -6,9 +6,9 @@ export const environment = {
   production: false,
   apiServerUrl: 'http://127.0.0.1:5000', // the running FLASK api server url
   auth0: {
-    url: 'dev-zuydypff.us.auth0', // the auth0 domain prefix
-    audience: 'coffee', // the audience set for the auth0 app
-    clientId: 'FpdBVoRTRTNHxHe4pfyk79mPG6uxOai7', // the client id generated for the auth0 app
-    callbackURL: 'http://127.0.0.1:5000', // the base url of the running ionic application. 
+    url: 'dev-z-5vcck9.us.auth0', // the auth0 domain prefix
+    audience: 'drinks', // the audience set for the auth0 app
+    clientId: 'lZeBYPKa0nSVOOiIDIEZ6bAnDe14KQid', // the client id generated for the auth0 app
+    callbackURL: 'https://127.0.0.1:8100/login/tabs/user-page', // the base url of the running ionic application. 
   }
 };
